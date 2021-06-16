1978e3f87443d326ff3d2bffe164f4aa6c72060a
kmatulud <kmatulud@student.wethinkcode.co.za>
2021-06-16 23:58:06 +0200

    more tests

--- factory.js
+++ factory.js
@@ -2,23 +2,14 @@ function Register() {
 	var regNum;
 	var town;
 	var regNumsArr = [];
+	var errorMsg = "";
 
 	function setRegNum(reg) {
 		regNum = reg;
 	}
-
 	function getRegNum() {
-		return regNum.toUpperCase().trim();
+		return regNum.toUpperCase();
 	}
-
-	function setTown(theTown) {
-		town = theTown;
-	}
-
-	function getTown() {
-		return town;
-	}
-
 	function setArr(regNum) {
 		regNumsArr = regNum;
 	}
@@ -27,11 +18,54 @@ function Register() {
 		return regNumsArr;
 	}
 
+	function setTown(townParam) {
+		town = townParam;
+	}
+
+	function getTown() {
+		return town;
+	}
+	function addRegNum(regNum) {
+		if (!regNumsArr.includes(regNum.toUpperCase())) {
+			if (regNum.length > 8) {
+				errorMsg = "Registration number is too long!";
+			} else {
+				if (regNum.length < 8) {
+					errorMsg = "Registration number is too short!should contain atleast 7 characters";
+				} else {
+					if (
+						regNum.startsWith("ca") ||
+						regNum.startsWith("cj") ||
+						regNum.startsWith("cy")
+					) {
+						errorMsg = "";
+						regNumsArr.push(regNum.toUpperCase());
+					} else {
+						errorMsg =
+							"Invalid registration number, must start with CA CJ or CY!";
+					}
+				}
+			}
+		} else {
+			errorMsg = "Registration number already exists";
+		}
+		if (regNum === "") {
+			errorMsg = "Please enter a registration number!";
+		}
+	
+	}
+
+	function getErrorMsg() {
+		return errorMsg;
+	}
+
 	function formatRegs(regs) {
 		return (
 			regs.substring(0, 2).toUpperCase() + "-" + regs.substring(2, 8) ||
 			regs.substring(0, 2).toUpperCase() + " " + regs.substring(2, 8)
-		)}
+		);
+	}
+
 	return {
 		setRegNum,
 		getRegNum,
@@ -40,5 +74,7 @@ function Register() {
 		setArr,
 		getArr,
 		formatRegs,
+		getErrorMsg,
+		addRegNum
 	};
 }
--- tests.js
+++ tests.js
@@ -26,11 +26,54 @@ describe("The Registration Project", function () {
 		assert.equal("CA-31221", register.formatRegs("CA31221"));
 	});
 
-	it("should accept a registration number format of this type: CJ9898", function () {
+	it("Should accept a registration number format of this type: CJ9898", function () {
 		assert.equal("CJ-9898", register.formatRegs("CJ9898"));
 	});
 
-	it("should accept a registration number format of this type: CY7878", function () {
+	it("Should accept a registration number format of this type: CY7878", function () {
 		assert.equal("CY-7878", register.formatRegs("CY7878"));
+	});
+	describe("Error messages", function () {
+		it("Should return an error message if no registration number has been added", function () {
+			register.addRegNum("");
+
+			assert.equal(
+				"Please enter a registration number!",
+				register.getErrorMsg()
+			);
+		});
+		it("Should return an error message if the registration number is too long", function () {
+			register.addRegNum("ca 1234412344")
+
+			assert.equal(
+				"Registration number is too long!",
+				register.getErrorMsg()
+			);
+		});
+		it("Should return an error message if the registration number is too long", function () {
+			register.addRegNum("ca 12");
+
+			assert.equal(
+				"Registration number is too short!should contain atleast 7 characters",
+				register.getErrorMsg()
+			);
+		});
+		it("Should return an error message if duplicate registrations have been added", function () {
+			register.addRegNum("ca 13324");
+			register.addRegNum("ca 13324");
+
+			assert.equal(
+				"Registration number already exists",
+				register.getErrorMsg()
+			);
+		});
+		it("Should return an error message registration numbers do not start with CA, Cy or CJ ", function () {
+			register.addRegNum("cm 13232")
+
+			assert.equal(
+				"Invalid registration number, must start with CA CJ or CY!",
+				register.getErrorMsg()
+			);
+		});
 	});
 });
